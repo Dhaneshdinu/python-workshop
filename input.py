3e240b2f2ad95431c0bdd265301f0f8caf570fc3
unknown <239095@TECH.WYB.AC.LK>
2025-03-26 19:39:10 +0530

    numbers

---
+++ input.py
@@ -0,0 +1,10 @@
+num=int(input("Enter your number:   "))
+print(num)
+
+name=str(input("Enter Your Name: "))
+print(name)
+
+num1=int(input("enter number1: "))
+num2=int(input("enter number2: "))
+print(num1)
+print(num2)
